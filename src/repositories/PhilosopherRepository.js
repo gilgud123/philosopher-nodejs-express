e839279543e1efb9accbc7de9b913cff44c6d497
Katya de Vries <Jekaterina.DeVries@iadvisegroep.eu>
2018-09-11 08:24:27 +0200

    Refactor Philosopher routes

--- src/repositories/PhilosopherRepository.js
+++ src/repositories/PhilosopherRepository.js
@@ -4,7 +4,7 @@ const toObject = (obj) => obj.toObject();
 const listToObjects = (list) => list.map(toObject);
 const getAll = () => Philosopher.find().then(listToObjects);
 const getById = (id) => Philosopher.findById(id).then(toObject);
-const getByName = (name) => Philosopher.find(name).then(toObject);
+const getByName = (name) => Philosopher.findOne({name: name}).then(toObject);
 const getByCategory = (c) => Philosopher.find( {categories: c} ).then(listToObjects);
 const create = (rawPhilosopher) => new Philosopher(rawPhilosopher).save().then(toObject);
 const remove = (id) => Philosopher.findByIdAndRemove(id);
--- src/resources/Philosopher.js
+++ src/resources/Philosopher.js
@@ -7,30 +7,33 @@ module.exports = (app) => {
     app.post('/philosopher', (req, res) => {
         PhilosopherService.create(req.body)
             .then((response) => res.status(200).send(response))
-            .catch(err => res.status(400).send('failed to create philosopher: ' + req.body.name));
+            .catch(err => res.status(400).send('Failed to create philosopher: ' + req.body.name));
     });
 
     app.patch('/philosopher/:id', (req, res) => {
-        Logger.log('info', `To be changed: ${req.body}`);
         PhilosopherService.patch(req.params.id, req.body)
             .then((response) => res.status(200).send(response))
-            .catch(err => res.status(400).send(`failed to change update description of the philosopher with ID: ${req.params.id}`));
+            .catch(err => res.status(400).send(`Failed to change update description of the philosopher with ID: ${req.params.id}`));
     });
 
     app.get('/philosopher', (req, res) => {
         PhilosopherService.getAll()
-            .then((response) => res.status(200).send(response));
+            .then((response) => res.status(200).send(response))
+            .catch(err => res.status(400).send(`Failed to get the philosophers.`));
     });
 
-    app.get('/philosoher/:id', (req, res) => {
+    app.get('/philosopher/:id', (req, res) => {
         PhilosopherService.getById(req.params.id)
-            .then((response) => res.status(200).send(response));
+            .then((response) => res.status(200).send(response))
+            .catch(err => res.status(400).send(`Philosopher with ID: ${req.params.id} does not exist in the database.`));
         }
     );
 
-    app.get('philosopher/:name', (req, res) => {
+    app.get('/philosopher/name/:name', (req, res) => {
+        Logger.log('info', `Philosopher by name: ${req.params.name}`);
             PhilosopherService.getByName(req.params.name)
-                .then((response) => res.status(200).send(response));
+                .then((response) => res.status(200).send(response))
+                .catch(err => res.status(400).send(`Philosopher by name: ${req.params.id} does not exist in the database.`));
         }
     );
 
