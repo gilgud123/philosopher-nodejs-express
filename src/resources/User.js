ef1c13efba234bb54db74a958d49be96db1cfe3c
Katya de Vries <Jekaterina.DeVries@iadvisegroep.eu>
2018-12-17 15:29:05 +0100

    refactor User routes

--- src/resources/User.js
+++ src/resources/User.js
@@ -2,40 +2,44 @@ const UserService = require('services/UserService');
 const Authorization = require('middleware/Authorization');
 
 const {modelValidator} = require('helpers/ValidationHelper');
+const ResponseHelper = require('helpers/ResponseHelper');
 const User = require('models/User');
 const Logger = require('helpers/LoggerHelper');
+const {
+    AuthorizeAdmin,
+    AuthorizeAdminOrLoggedInUser
+            } = require('middleware/Authorization');
 
 module.exports = (app) => {
 
-    app.post('/user', modelValidator(User), /*Authorization.AuthorizeAdmin,*/ (req, res) => {
-        UserService.create(req.body)
-            .then((response) => res.status(200).send(response))
-            .catch(err => res.status(400).send('Failed to create user: ' + req.body.name));
+    app.post('/user', modelValidator(User), AuthorizeAdmin, (req, res) => {
+        ResponseHelper.promiseResponseHandler(
+            req, res, UserService.create(req.body));
     });
 
-    app.patch('/user/:id', /*Authorization.AuthorizeAdminOrLoggedInUser,*/ (req, res) => {
-        UserService.patch(req.params.id, req.body)
-            .then((response) => res.status(200).send(response))
-            .catch(err => res.status(400).send(`Failed to change update the user with ID: ${req.params.id}`));
+    app.patch('/user/:id', AuthorizeAdminOrLoggedInUser, (req, res) => {
+        ResponseHelper.promiseResponseHandler(
+            req, res, UserService.patch(req.params.id, req.body));
     });
 
-    app.get('/user', /*Authorization.AuthorizeAdmin,*/ (req, res) => {
-        UserService.getAll()
-            .then((response) => res.status(200).send(response))
-            .catch(err => res.status(400).send(`Failed to get the users.`));
+    app.get('/user', AuthorizeAdmin, (req, res) => {
+        ResponseHelper.promiseResponseHandler(
+            req, res, UserService.getAll());
     });
 
-    app.get('/user/:id', /*Authorization.AuthorizeAdminOrLoggedInUser, */(req, res) => {
-            UserService.getById(req.params.id)
-                .then((response) => res.status(200).send(response))
-                .catch(err => res.status(400).send(`User with ID: ${req.params.id} does not exist in the database.`));
-        }
-    );
+    app.get('/user/:id', AuthorizeAdminOrLoggedInUser, (req, res) => {
+        ResponseHelper.promiseResponseHandler(
+            req, res, UserService.getById(req.params.id));
+    });
 
-    app.delete('/user/:id', /*Authorization.AuthorizeAdminOrLoggedInUser,*/ (req, res) => {
-            UserService.remove(req.params.id)
-                .then((response) => res.status(200).send(response))
-                .catch(err => res.status(400).send(`failed to delete user with ID: ${req.params.id}`));
-        }
-    );
+    app.delete('/user/:id', AuthorizeAdminOrLoggedInUser, (req, res) => {
+        ResponseHelper.promiseResponseHandler(
+            req, res, UserService.remove(req.params.id));
+    });
+
+    app.delete('/user/:id/hardremove', AuthorizeAdmin, (req, res) => {
+        ResponseHelper.promiseResponseHandler(
+            req, res, UserService.hardRemove(req.params.id, req.requestor));
+    });
+
 };
