c1c378acf5c30c70fcd15314192d72afb049dbf3
Katya de Vries <Jekaterina.DeVries@iadvisegroep.eu>
2019-01-17 16:11:59 +0100

    add confirm mail template, refactor MailConfigMailer helper

--- src/helpers/MailConfigMailer.js
+++ src/helpers/MailConfigMailer.js
@@ -3,6 +3,7 @@ const handlebars = require('handlebars');
 const email = 'gilgud123@gmail.com';
 const password = 'Prutser123';
 const {resetPasswordTemplate} = require('mail-templates/Reset');
+const {confirmPasswordTemplate} = require('mail-templates/Confirm');
 
 const Logger = require('helpers/LoggerHelper');
 
@@ -29,18 +30,17 @@ const mail = (sendToEmail, subject, html) => {
     });
 };
 
-/*const confirmationMail = (user) => {
+const confirmationMail = (user) => {
     const data = {
         lastName: user.lastName,
         firstName: user.firstName
     };
-    const handledTemplate = handlebars.compile(confirmationTemplate);
+    const handledTemplate = handlebars.compile(confirmPasswordTemplate);
     const emailBody = handledTemplate(data);
-    //const sendToEmail = process.env.APP_ID ? user.email : ['katya.devries@jstack.eu', 'k.devries@scarlet.be'];
-    const subject = `Your jstack registration was successful!`;
+    const subject = `Your registration was successful!`;
 
     return mail(user.email, subject, emailBody);
-};*/
+};
 
 const resetPasswordMail = (user, token) => {
     const url = 'http://localhost:8081/password/reset/'+token;
@@ -53,4 +53,4 @@ const resetPasswordMail = (user, token) => {
     return mail(user.email, subject, emailBody);
 };
 
-module.exports = { resetPasswordMail };
+module.exports = { resetPasswordMail, confirmationMail };
